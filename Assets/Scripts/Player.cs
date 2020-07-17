789652ece624c271702c066b8c0f6f30664076ad
Prashantha SP <prashanth.sp98@gmail.com>
2020-07-17 16:58:19 +0530

    Game is completed on basic checkpoint, fixed some rotation speed issues

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    #region serializedVariables
+    [SerializeField] float movementSpeed = 500f;
+    private SceneHandler sceneHandler = default;
+    #endregion
+
+
+    #region privateVariables
+    private float movementManager;
+    private float movement = 0f;
+    private float speed = 0f;
+    private Vector3 touchPosition = Vector3.zero;
+    #endregion
+
+
+    void Start()
+    {
+        sceneHandler = FindObjectOfType<SceneHandler>();
+        speed = movementSpeed;
+    }
+
+    void Update()
+    {
+        TouchInputData();
+        movement = touchPosition.x;
+        movementManager = (int)(-movement * Time.deltaTime * speed);
+        transform.RotateAround(Vector3.zero, Vector3.forward, movementManager);
+        TouchInputData();
+
+    }
+
+    private void TouchInputData()
+    {
+
+        // #if UNITY_EDITOR
+        //         touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //         if (Input.GetMouseButtonDown(0))
+        //         {
+        //             // Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        //         }
+        // #endif
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+            {
+                touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                if (touchPosition.x < -1.9 || touchPosition.x > 1.9)
+                {
+                    speed = movementSpeed - (movementSpeed / 2 );
+                    Debug.Log(speed);
+                    Debug.Log(touchPosition);
+                }
+                Debug.Log(touch.type);
+            }
+        }
+        else
+            touchPosition = Vector3.zero;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) => sceneHandler.GameOverScene();
+}
---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    
+    public void GameOverScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void GameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
